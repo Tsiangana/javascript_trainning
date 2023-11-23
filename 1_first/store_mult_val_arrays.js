ae9adddc2935ca249d3d4e45150e3a9468e98f03
Tsiangana <pedrozau250@gmail.com>
2023-11-23 18:53:50 +0000

    My Fouth day learning JS from zero again

---
+++ 1_first/store_mult_val_arrays.js
@@ -0,0 +1,43 @@
+
+
+    // Example
+    var ourArray = ["John", 23];
+
+
+
+    // Nested Array
+    var oneOurArray = [["the universe", 42], ["everything", 101010]];
+
+
+
+    // Access Array Data with Indexes
+    var ourMultArray = [50,60,70];
+    var ourData = ourMultArray[0];
+
+
+
+    // Modify Array Data with Indexes
+    var modifyOurArray = [18,64,99];
+    modifyOurArray[1] = 45;  // modifyOurArray now equals [18,45,99]
+
+
+
+    // Access Multi-Dimensional Arrays with Indexes
+    var myDimentionalArrays = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+    var myData = myDimentionalArrays[2][1];
+
+
+
+    // Manipulate Arrays with push()
+    var pushOurArray = ["Stimpson", "J", "cat"];
+    pushOurArray.push(["happy", "joy"]);
+    // pushOurArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+
+
+    // Manipulate Arrays with pop()
+
+
+
+    // console.log(pushOurArray)
