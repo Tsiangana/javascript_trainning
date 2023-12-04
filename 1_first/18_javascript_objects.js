b4316574c0785751ebddc79cd28a261c01d6c6c6
Tsiangana <pedrozau250@gmail.com>
2023-12-04 20:21:14 +0000

    my therth day learning Js from zero again

--- 1_first/18_javascript_objects.js
+++ 1_first/18_javascript_objects.js
@@ -253,7 +253,55 @@
 
 
     // Record Collection
-    
+    var collection = {
+        "2548": {
+            "album": "Slippery when wet",
+            "artist": "Bon Jovi",
+            "tracks": [
+                "Let It Rock",
+                "You Give Love a Bad Name"
+            ]
+        },
+        "2468": {
+            "album": "1999",
+            "artist": "Princi",
+            "tracks": [
+                "1999",
+                "Little Red corvette"
+            ]
+        },
+        "1245": {
+            "artist": "Robert Palmer",
+            "tracks": []
+        },
+        "5439": {
+            "album": "ABBA Gold"
+        }
+    };
+    // Keep a copy of the collection for tests
+    var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+    // Only change code below this line
+    function updateRecords(id, prop, value) {
+        if (value === "") {
+            delete collection[id][prop];
+        }else if (prop === "tracks") {
+            collection[id][prop] = collection[id][prop] || [];
+            collection[id][prop].push(value);
+        }else {
+            collection[id][prop] = value;
+        }
+
+        return collection;
+    }
+
+    // Alter values below to test your code
+    updateRecords(2468, "tracks", "test");
+    // console.log(updateRecords(5439, "artist", "ABBA"));
+
+
+
+    // Iterate with while Loops
     
 
 
